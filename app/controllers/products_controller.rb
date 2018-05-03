c936616d7773cf82cfa698610e59be077ede22f7
Anna-Marie Cooper and David Lewis <anna-marie@>
2018-05-03 09:45:08 -0700

    figure out how to fucking display each description like a fucking bo579 :money:

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,4 +4,13 @@ class ProductsController < ApplicationController
     @products = Product.all
     @order_item = current_order.order_items.new
   end
+
+  def show
+    @products = Product.all
+    @product = Product.find(params[:id])
+    respond_to do |format|
+      format.html { redirect_to products_path }
+      format.js
+    end
+  end
 end
